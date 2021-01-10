0041771eb3ac9c1db19137fc6977b524310fb95c
Chandrakant Singh <aryainformationsystems@gmail.com>
2021-01-10 22:54:04 +0530

    Fixed organizations

--- src/app/services/organization.service.ts
+++ src/app/services/organization.service.ts
@@ -1,9 +1,71 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiResponse } from '../classes/api-response';
+import { EnvService } from './env.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrganizationService {
+  private baseUrl: string;
+  constructor(private httpClient: HttpClient, private envService: EnvService) {
+    this.baseUrl = envService.apiServer + '/organization';
+  }
 
-  constructor() { }
+  getAll(): Observable<ApiResponse> {
+    return this.httpClient.get<ApiResponse>(this.baseUrl, {
+      headers: { secure: 'true' },
+    });
+  }
+
+  create(organizationDetails: any): Observable<ApiResponse> {
+    return this.httpClient.post<ApiResponse>(
+      this.baseUrl,
+      organizationDetails,
+      { headers: { secure: 'true' } }
+    );
+  }
+
+  getById(id: string): Observable<ApiResponse> {
+    return this.httpClient.get<ApiResponse>(this.baseUrl + '/' + id, {
+      headers: { secure: 'true' },
+    });
+  }
+
+  delete(id: string): Observable<ApiResponse> {
+    return this.httpClient.delete<ApiResponse>(this.baseUrl + '/' + id, {
+      headers: { secure: 'true' },
+    });
+  }
+
+  deleteMany(ids: string[]): Observable<ApiResponse> {
+    return this.httpClient.post<ApiResponse>(
+      this.baseUrl + '/delete',
+      {
+        ids,
+      },
+      { headers: { secure: 'true' } }
+    );
+  }
+
+  update(id: string, organizationDetails: any): Observable<ApiResponse> {
+    return this.httpClient.put<ApiResponse>(
+      this.baseUrl + '/' + id,
+      organizationDetails,
+      { headers: { secure: 'true' } }
+    );
+  }
+  getNameAndId(): Observable<ApiResponse> {
+    return this.httpClient.get<ApiResponse>(this.baseUrl, {
+      headers: { secure: 'true' },
+    });
+  }
+
+  totalOrganizations(): Observable<ApiResponse> {
+    return this.httpClient.get<ApiResponse>(
+      this.baseUrl + '/totalOrganizations',
+      { headers: { secure: 'true' } }
+    );
+  }
 }
